ddfddbd7e61d27f33a9ae6dc18f3fc1f495276ef
Minh Quang <hikari0901@gmail.com>
2018-02-22 00:43:44 +0700

    aaa

---
+++ app/frontend/views/contact.php
@@ -0,0 +1,8 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']. '/libs/bootstrap.php');
+//echo $root.'  root';
+$main_content = $root.'/app/frontend/views/main_contents/contact.php';
+include ($root.'/app/frontend/views/layout.php');
+?>
+
+
