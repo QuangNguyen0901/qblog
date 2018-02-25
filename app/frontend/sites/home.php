a0a21459eadf0285ebb549d34b084402cb2243ab
Minh Quang <hikari0901@gmail.com>
2018-02-26 00:14:11 +0700

    aa

--- app/frontend/sites/home.php
+++ app/frontend/sites/home.php
@@ -2,14 +2,14 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-            <?php foreach ($row as $row1) { ?>
+            <?php foreach ($row as $post) { ?>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="?m=post&a=view&id=<?php echo $post['id'] ?>">
                         <h2 class="post-title">
-                            <?php echo $row1['id'] ?>
+                            <?php echo $post['id'] ?>
                         </h2>
                         <h3 class="post-subtitle">
-                            <?php echo $row1['name'] ?>
+                            <?php echo $post['name'] ?>
                         </h3>
                     </a>
                     <p class="post-meta">Posted by
