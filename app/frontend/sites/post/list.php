7701776283138393c28a581e9fd22a81c264fa56
QuangNguyen <hikari0901@gmail.com>
2018-03-01 18:37:34 +0700

    aaa

--- app/frontend/sites/post/list.php
+++ app/frontend/sites/post/list.php
@@ -1,3 +1,20 @@
 <?php
+//echo "list all post";
 
-echo "list all post";
+//?m-post&a=list&page=3
+//?m-post&a=list
+
+$page = (isset($_GET['page']) ? $_GET['page'] : 1);
+$limit = 3;
+$offset = ($page - 1)*$limit;
+$db= new Database();
+
+$sql = "SELECT * FROM post WHERE title !='about' AND published =1 ORDER BY id DESC LIMIT $limit OFFSET $offset";
+$db->query($sql);
+
+$row = $db->findAll();
+//print_r($row);
+
+$background_image ='/assets/img/home-bg.jpg';
+$main_content = $root.'/app/frontend/views/post/list.php';
+include ($root.'/app/frontend/views/layout.php');
---
+++ app/frontend/views/post/list.php
@@ -0,0 +1,23 @@
+<!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <?php foreach ($row as $post) { ?>
+                <div class="post-preview">
+                    <a href="?m=post&a=view&id=<?php echo $post['id'] ?>">
+                        <h2 class="post-title">
+                            <?php echo $post['title'] ?>
+                        </h2>
+                        <h3 class="post-subtitle">
+                            <?php echo $post['short_des'] ?>
+                        </h3>
+                    </a>
+                    <p class="post-meta">Posted on <?php echo $post['created_at'] ?></p>
+                </div>
+                <hr>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</div>
