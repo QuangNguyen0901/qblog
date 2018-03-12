215e877b2669516d4b6140f9c8b6c471cff2fe5f
QuangNguyen <hikari0901@gmail.com>
2018-03-12 08:53:41 +0700

    may tinh cong ty app/frontend/sites/contact.php

--- app/frontend/sites/contact.php
+++ app/frontend/sites/contact.php
@@ -4,10 +4,10 @@ $background_image ='/assets/img/contact-bg.jpg';
 $main_content = $root.'/app/frontend/views/contact.php';
 include ($root.'/app/frontend/views/layout.php');
 
-$sender_email = $_POST['name'];
-echo $sender_email;
+$sender_name    = $_POST['name'];
+$sender_email   = $_POST['email'];
+$sender_phone   = $_POST['phone'];
+$sender_massage = $_POST['massage'];
 
-
-
-//include ($root.'/libs/send_mail.php');
+include ($root.'/libs/send_mail.php');
 ?>
