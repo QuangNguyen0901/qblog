cbafb5da010c5cc4d509ee10dbbb909fad8ac137
QuangNguyen <hikari0901@gmail.com>
2018-06-11 19:55:23 +0700

    sort

--- app/backend/views/newword/word_list.php
+++ app/backend/views/newword/word_list.php
@@ -48,9 +48,9 @@ if (!empty($_SESSION['flash'])) {
                 <thead>
                 <tr>
                     <th></th>
-                    <th>Words <span class="fa fa-sort" style="color: lightgrey;float:right"></span> </th>
-                    <th>Mean<span class="fa fa-sort-up"></th>
-                    <th>Description<span class="fa fa-sort-down"></th>
+                    <th>Words <span class="fa fa-sort no-sort" id="cl1" ></span> </th>
+                    <th>Mean<span class="fa fa-sort-up yes-sort" id="cl2"></th>
+                    <th>Description<span class="fa fa-sort-down yes-sort" id="cl3"></th>
                     <th>View image</th>
                     <th>Action</th>
                 </tr>
@@ -128,8 +128,6 @@ if (!empty($_SESSION['flash'])) {
             $(this).closest('tr').find("input").removeAttr('readonly');
             $(this).closest('tr').find("input").addClass('editing');
 
-//            console.log($(this).parent().find('.cancel'));
-
             $(this).parent().find('.cancel').css("display", "inline");
             $(this).parent().find('.save').css("display", "inline");
 
@@ -159,18 +157,45 @@ if (!empty($_SESSION['flash'])) {
 
             $(this).parent().find('.cancel').css("display", "none");
             $(this).css("display", "none");
+            var this1 = $(this); // TRUYEN bien this vao function khac
 
 
             $.post('/app/backend/sites/newword/word_edit.php', {
-                // word_id:$(this).attr("id")
                 word_id: $(this).attr("id"),
                 word: $(this).closest('tr').find(".word").val(),
                 mean: $(this).closest('tr').find(".mean").val(),
                 description: $(this).closest('tr').find(".description").val(),
                 image: $(this).closest('tr').find(".image").val()
             }, function (result) {
-
+                var result_array=  jQuery.parseJSON(result);
+                console.log(result_array);
+                console.log(this1.closest('tr').find('.word').val());
+                this1.closest('tr').find(".word").val(result_array['word']);
+                this1.closest('tr').find("input .mean").val(result_array['mean']);
+                this1.closest('tr').find("input .description").val(result_array['description']);
+                this1.closest('tr').find("input .image").val(result_array['image']);
             });
+
+        });
+
+        $('.fa').click(function (event) {
+            event.preventDefault();
+            if ($(this).hasClass('no-sort')){
+                $(this).removeClass('no-sort');
+                $(this).removeClass('fa-sort');
+                $(this).addClass('fa-sort-down');
+                $(this).addClass('yes-sort');
+            } else {
+                if ($(this).hasClass('fa-sort-down')){
+                    $(this).removeClass('fa-sort-down');
+                    $(this).addClass('fa-sort-up');
+                }else {
+                    $(this).removeClass('fa-sort-up');
+                    $(this).addClass('fa-sort-down');
+                }
+            }
+            var this_id = $(this).attr('id');
+            $('.fa').not('#'+this_id).removeClass().addClass('fa fa-sort no-sort');
         });
     });
 
